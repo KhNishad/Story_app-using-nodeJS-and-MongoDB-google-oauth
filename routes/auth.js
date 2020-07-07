def69c8e959ba1f56c794c6f1a75b437bdc7a69d
khNishad <kkhnishad@gmail.com>
2020-07-07 21:46:55 +0600

    navbar

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,9 @@ router.get(
 
 //    Logout user
 //    /auth/logout
-// router.get('/logout', (req, res) => {
-//     req.logout()
-//     res.redirect('/')
-// })
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
 
 module.exports = router
